1153da6d9187d4bf0fe6efe72908c66386d63e4b
jlfbueno <jlfbueno@gmail.com>
2025-09-30 11:40:24 +0200

    deploy

---
+++ assets/status-tap-BHsn14Yo.js
@@ -0,0 +1,3 @@
+import{b as a,f as i,e as c,h as d,s as l}from"./index-js-nJD9c.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const h=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{h as startStatusTap};
